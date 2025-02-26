06221c1fb808dabb8ee69907612254d2c977c1bf
DawnProspect <rzaljabar@gmail.com>
2025-02-26 13:21:23 +0700

    setup JWT Helpers

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,9 +1,13 @@
 const { database } = require("../config/mongodb")
 const { ObjectId } = require('mongodb')
+const { hashPassword, comparePassword } = require("../helpers/bcrypt")
 
 
 class UserModel {
-
+    // * Ambil collection users
+    static collection() {
+        return database.collection("users")
+    }
 }
 
 module.exports = UserModel
