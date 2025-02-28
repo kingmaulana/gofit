4eb9eaea6d8bc60fe534096340c95c6e57505488
windraseptamadya-asrah <windra.asrah@outlook.com>
2025-02-28 16:33:10 +0700

    refactor Step1 component to include next button and handle navigation

--- client/screens/onboarding/step1.jsx
+++ client/screens/onboarding/step1.jsx
@@ -1,27 +1,35 @@
 import OnboardingLayout from "@/components/screens/onboarding-layout";
 import {TextInput, View, Text} from "react-native";
 import {useFormContext} from "react-hook-form";
-import {Button} from "@/components/ui/button";
+import {Button, ButtonText} from "@/components/ui/button";
 
-export default function Step1() {
+export default function Step1({onNext}) {
   const form = useFormContext();
+
+  const handleNext = () => {
+    // TODO: Implement username check
+    onNext();
+  }
+
   return <OnboardingLayout>
-    <View className="p-4 flex-1 flex-col gap-2">
-      <View>
-        <Text className="text-white">What is your name?</Text>
-        <TextInput className="px-4 py-2 border border-white text-white rounded-md"/>
+      <View className="p-4 flex-col gap-2">
+        <View>
+          <Text className="text-white">What is your name?</Text>
+          <TextInput className="p-2 border border-white text-white rounded-md"/>
+        </View>
+        <View>
+          <Text className="text-white">What is your email?</Text>
+          <TextInput className="p-2 border border-white text-white rounded-md"/>
+        </View>
+        <View>
+          <Text className="text-white">Can you type your password</Text>
+          <TextInput className="p-2 border border-white text-white rounded-md"/>
+        </View>
       </View>
-      <View>
-        <Text className="text-white">What is your email?</Text>
-        <TextInput className="px-4 py-2 border border-white text-white rounded-md"/>
+      <View className="flex-grow-0">
+        <Button action="secondary" onPress={handleNext}>
+          <ButtonText>Next</ButtonText>
+        </Button>
       </View>
-      <View>
-        <Text className="text-white">Can you type your password</Text>
-        <TextInput className="px-4 py-2 border border-white text-white rounded-md"/>
-      </View>
-    </View>
-    <View>
-      <Button></Button>
-    </View>
   </OnboardingLayout>
 }
