b90f460cf085edb4722b82544adc778d04691058
DawnProspect <rzaljabar@gmail.com>
2025-03-03 23:23:25 +0700

    Add profile section, data is still hardcode

--- client/navigations/TabNavigator.js
+++ client/navigations/TabNavigator.js
@@ -12,6 +12,7 @@ import CreateCategoryPage from '@/screens/CreateCategory';
 
 import {TouchableOpacity, View} from "react-native";
 import {AuthContext} from "@/helpers/auth-context";
+import ProfileScreen from '@/screens/Profile';
 
 
 const Tab = createBottomTabNavigator();
@@ -36,6 +37,8 @@ export default function TabNavigator() {
             iconName = focused ? 'list' : 'list-outline';
           } else if (route.name === 'AddExercisePage') {
             iconName = focused ? 'add-circle' : 'add-circle-outline';
+          } else if (route.name === 'Profile') {
+            iconName = focused ? 'person' : 'person-outline';
           }
           return <Ionicons name={iconName} size={size} color={color}/>;
         },
@@ -64,6 +67,7 @@ export default function TabNavigator() {
       <Tab.Screen name="CustomCategory" component={CustomCategory} />
       <Tab.Screen name="AddExercisePage" component={AddExercisePage} />
       <Tab.Screen name="CreateCategoryPage" component={CreateCategoryPage} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
 
     </Tab.Navigator>
   )
