ba3a708d29f99f88a10aa4eee9a95b65eabc905d
windraseptamadya-asrah <windra.asrah@outlook.com>
2025-03-03 23:27:36 +0700

    added query to get all categories

--- client/screens/Home.js
+++ client/screens/Home.js
@@ -1,34 +1,46 @@
-import { Card } from "@/components/ui/card"
-import { Heading } from "@/components/ui/heading"
-import { HStack } from "@/components/ui/hstack"
-import { VStack } from "@/components/ui/vstack"
-import { Image } from "@/components/ui/image"
-import { Link, LinkText } from "@/components/ui/link"
-import { Text } from "@/components/ui/text"
-import { Icon, ArrowRightIcon, DumbbellIcon, BellIcon } from "@/components/ui/icon"
-import { ScrollView, TouchableWithoutFeedback } from "react-native"
-import { Box } from "@/components/ui/box"
-import {
-  Avatar,
-  AvatarBadge,
-  AvatarFallbackText,
-  AvatarImage,
-} from "@/components/ui/avatar"
-import { Button } from "@/components/ui/button"
-import { Divider } from "@/components/ui/divider"
-import { useNavigation } from "@react-navigation/native"
+import {Card} from "@/components/ui/card"
+import {Heading} from "@/components/ui/heading"
+import {HStack} from "@/components/ui/hstack"
+import {VStack} from "@/components/ui/vstack"
+import {Image} from "@/components/ui/image"
+import {Link, LinkText} from "@/components/ui/link"
+import {Text} from "@/components/ui/text"
+import {ArrowRightIcon, Icon} from "@/components/ui/icon"
+import {ScrollView, TouchableWithoutFeedback} from "react-native"
+import {Box} from "@/components/ui/box"
+import {Button} from "@/components/ui/button"
+import {Divider} from "@/components/ui/divider"
+import {useNavigation} from "@react-navigation/native"
+import {gql, useQuery} from "@apollo/client";
+
+const GET_CATEGORY = gql(`
+  query GetCategories {
+      exerciseCategories {
+          name,
+          duration,
+          _id
+      }
+  }
+`)
 
 export default function Home() {
-
+  const { data: allExerciseCategory, loading, error } = useQuery(GET_CATEGORY);
   const navigation = useNavigation()
 
+  // TODO: fetch from db
+  const newExerciseCategories = allExerciseCategory?.map((category) => ({
+    name: category.name,
+    duration: category.duration,
+    image: `https://image.pollinations.ai/prompt/a%20workout%20category%20called%20${category.name}%20in%20black%20and%20white%20500x500?nologo=true`
+  }));
   const exerciseCategories = [
-    { name: "Strength", image: "https://image.pollinations.ai/prompt/weightlifting%20black%20and%20white%20500x500" },
-    { name: "Cardio", image: "https://image.pollinations.ai/prompt/running%20black%20and%20white%20500x500" },
-    { name: "Flexibility", image: "https://image.pollinations.ai/prompt/yoga%20black%20and%20white%20500x500" },
-    { name: "HIIT", image: "https://image.pollinations.ai/prompt/hiit%20workout%20black%20and%20white%20500x500" }
+    { name: "Strength", image: "https://image.pollinations.ai/prompt/weightlifting%20black%20and%20white%20500x500?nologo=true" },
+    { name: "Cardio", image: "https://image.pollinations.ai/prompt/running%20black%20and%20white%20500x500?nologo=true" },
+    { name: "Flexibility", image: "https://image.pollinations.ai/prompt/yoga%20black%20and%20white%20500x500?nologo=true" },
+    { name: "HIIT", image: "https://image.pollinations.ai/prompt/hiit%20workout%20black%20and%20white%20500x500?nologo=true" }
   ];
 
+  // TODO: genAI thing
   const workoutsOfDay = [
     { name: "Full Body Power", difficulty: "Advanced", time: "45 min", image: "https://image.pollinations.ai/prompt/muscle%20man20gym%20500x500" },
     { name: "Core Crusher", difficulty: "Intermediate", time: "30 min", image: "https://image.pollinations.ai/prompt/core%20workout%20black%20and%20white%20500x500" }
@@ -39,7 +51,7 @@ export default function Home() {
       <Box className="px-4 py-2">
         <HStack className="justify-between items-center mb-3">
           <Heading size="md" className="">Your Workouts</Heading>
-          <Link>
+          <Link onPress={() => navigation.navigate("Personal")}>
             <HStack className="items-center">
               <LinkText size="sm" className="text-gray-600 no-underline">View All</LinkText>
               <Icon as={ArrowRightIcon} size="sm" className="text-gray-600" />
